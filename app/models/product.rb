8b55b8a8fef4b19d12639581d9b5d9d44c10c462
Christopher Manouvrier <cmanou@mac.com>
2012-08-14 17:24:42 +1000

    Attempted nested views didn't work

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   has_many :stock_levels
   has_many :stock_locations, :through => :stock_levels
   attr_accessible :cost, :description, :name, :price, :barcode
+  accepts_nested_attributes_for :stock_levels
 end
