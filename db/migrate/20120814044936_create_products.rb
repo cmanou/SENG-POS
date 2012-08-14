158ef72133ce796c66855486d7688c2331dde25f
Dylan Kelly <dylank@cse.unsw.edu.au>
2012-08-14 15:42:28 +1000

    added barcode to products

--- db/migrate/20120814044936_create_products.rb
+++ db/migrate/20120814044936_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.string :barcode
       t.string :name
       t.text :description
       t.decimal :cost
