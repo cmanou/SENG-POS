322a4e207b1e25e9b6b2954d8a01529928a27ba8
Christopher Manouvrier <cmanou@mac.com>
2012-08-17 12:13:11 +1000

    Fixed Javascript problems

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+//= require jquery
+//= require jquery_ujs
+
+$(document).ready(function() {
+    $('.global-flash .alert').click(function() {
+        $(this).slideUp('fast');
+    });
+    
+    
+      // Fix input element click problem for login dropdown
+      $('.dropdown input, .dropdown label, .dropdown-menu').click(function(e) {
+        e.stopPropagation();
+      });
+
+
+    var timeout = null;
+	var initialMargin = parseInt($(".hide-this-shit").css("margin-top"));
+	 
+	$(".hide-this-shit").hover(
+		function() {
+			if (timeout) {
+				clearTimeout(timeout);
+				timeout = null;
+			}
+			$(this).animate({ marginTop: 0 }, 'fast');
+		},
+		function() {
+			var menuBar = $(this);
+			timeout = setTimeout(function() {
+				timeout = null;
+				menuBar.animate({ marginTop: initialMargin }, 'fast');
+			}, 1000);
+		}
+	);
+});
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_many :stock_levels
+  has_many :stock_levels, :dependent => :destroy
   has_many :stock_locations, :through => :stock_levels
   attr_accessible :cost, :description, :name, :price, :barcode
 
