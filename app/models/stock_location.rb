5203c66fd6b7d157f1901bbf2b818355ec0b625f
Christopher Manouvrier <cmanou@mac.com>
2012-08-21 15:46:30 +1000

    Style fixes + destroy fix

--- app/models/stock_location.rb
+++ app/models/stock_location.rb
@@ -1,5 +1,5 @@
 class StockLocation < ActiveRecord::Base
-  has_many :stock_levels
+  has_many :stock_levels, :dependent => :destroy
   has_many :products, :through => :stock_levels
   attr_accessible :name
 end
