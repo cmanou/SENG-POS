6a58248ffdbccf71814a7d28689484ce908cfe72
Dylan Kelly <dylank@cse.unsw.edu.au>
2012-09-25 08:48:54 +1000

    added fields for products from class diagram

--- db/migrate/20120814044936_create_products.rb
+++ db/migrate/20120814044936_create_products.rb
@@ -1,13 +1,18 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.references :supplier
       t.string :barcode
       t.string :name
+      t.string :brand
+      t.string :size
       t.text :description
       t.decimal :cost
       t.decimal :price
-
+      t.string :active
+      
       t.timestamps
     end
+      add_index :products, :barcode
   end
 end
