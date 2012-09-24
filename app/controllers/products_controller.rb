11487a1288e4c45a3d0a482004592f6c44f5e65c
Dylan Kelly <dylank@cse.unsw.edu.au>
2012-09-25 09:46:43 +1000

    included size in checkout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -53,15 +53,10 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     #raise params.inspect
-
+    @supplier = Supplier.find_by_id(Integer(params[:product][:supplier]))
+    params[:product][:supplier] = @supplier
     @product = Product.new(params[:product])
     
-    @supplier = Supplier.find(Integer(params[:product][:supplier]))
-
-    params[:product][:supplier] = @supplier
-
-
-
     params[:stock_level].each do |sl_id, sl|
       @product.stock_levels.build(:stock_location => StockLocation.find(sl_id.to_i), :quanity =>sl[:quanity], :threshold => sl[:threshold])
     end
