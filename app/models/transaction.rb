8474dce4052c4a68c8c0e3b42f5c8e26aeac9c76
Lasath Fernando <kde@lasath.org>
2012-09-28 09:36:08 +1000

    Add validatations for Transaction model
    I think they're consitent with the invarients.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,11 @@ class Transaction < ActiveRecord::Base
   belongs_to :sale
 
   attr_accessible :amount, :approved, :method, :sale
+
+  validates :amount, :presence => true,
+      :exclusion => { :in => [0], :message => "Must be non-zero amount." }
+
+  validates :method, :presence => true, :inclusion => { :in => ['Cash', 'Other']}
+
+  validates_associated :sale
 end
