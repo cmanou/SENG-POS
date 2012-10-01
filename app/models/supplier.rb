272b07fab24a48418959da43a53318d01cf30546
Christopher Manouvrier <cmanou@mac.com>
2012-10-01 12:22:37 +1000

    Supplier validations

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,8 @@
 class Supplier < ActiveRecord::Base
   has_many :products
   attr_accessible :contact_number, :contact_person, :name
+
+  validates :name, :contact_number, :contact_person, :presence => true
+  validates :name, :uniqueness => true
+
 end
