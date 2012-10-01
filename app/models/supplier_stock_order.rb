b86fe4bf42fea9ed38c56797b9004d65fa98ad7e
Christopher Manouvrier <cmanou@mac.com>
2012-10-01 12:27:49 +1000

    Supplier Stock Order validation

--- app/models/supplier_stock_order.rb
+++ app/models/supplier_stock_order.rb
@@ -6,6 +6,10 @@ class SupplierStockOrder < ActiveRecord::Base
        :inclusion  => { :in => [ 'Created', 'Processed', 'Completed'],
        :message    => "%{value} is not a valid status" }
 
+  validates :product, :quantity, :status, :presence => true
+  validates :quantity, :numericality => {:greater_than => 0}
+
+
   def total
   	quantity * product.cost
   end
