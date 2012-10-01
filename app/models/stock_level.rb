a9b6a91c98e569e700f56d3a8c8b98aefbf03319
Christopher Manouvrier <cmanou@mac.com>
2012-10-01 12:09:27 +1000

    Stock Level Validators
    treating threshold as max-threshold from model

--- app/models/stock_level.rb
+++ app/models/stock_level.rb
@@ -4,6 +4,11 @@ class StockLevel < ActiveRecord::Base
   attr_accessible :quantity, :threshold, :product, :stock_location
 
 
+#threshold is max threshold from eventb model
+  validates :quantity, :threshold, :numericality => {:greater_than_or_equal_to => 0}
+  validates :quantity, :numericality => {:less_than_or_equal_to => :threshold}
+
+
   def below 
   	quantity < threshold
   end
