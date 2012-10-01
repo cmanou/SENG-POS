01907e4be50cafdf96d29283229009f49b124a35
Christopher Manouvrier <cmanou@mac.com>
2012-10-01 12:20:55 +1000

    Stock transfer validation

--- app/models/stock_transfer.rb
+++ app/models/stock_transfer.rb
@@ -2,4 +2,8 @@ class StockTransfer < ActiveRecord::Base
   belongs_to :product
   belongs_to :stock_location
   attr_accessible :product, :stock_location, :complete, :quantity
+
+   validates :product, :stock_location, :quantity, :presence => true
+   validates :quantity, :numericality => {:greater_than => 0}
+
 end
