df2da29d5af6708edb3b4f4f61182af8a49a6f8b
Christopher Manouvrier <cmanou@mac.com>
2012-10-03 17:54:41 +1000

    Sale Item event b comment

--- app/models/sale_item.rb
+++ app/models/sale_item.rb
@@ -1,8 +1,10 @@
 class SaleItem < ActiveRecord::Base
+  #Event-B: CART = PRODUCT ⇸ ℕ
+  #Summary: This class represents the cart set from our model
   belongs_to :sale
   belongs_to :product
 
   attr_accessible :sale, :product, :quantity, :sub_total
-
+#This has to be fixed
   #validates :product_active, :inclusion  => { :in => [true, false], :message    => "%{product} is not active" }
 end
