ae0281699fc816156dd38df1f07c1d2a705bbf90
Christopher Manouvrier <cmanou@mac.com>
2012-10-03 18:11:01 +1000

    Stock_level event-b

--- app/models/stock_level.rb
+++ app/models/stock_level.rb
@@ -1,12 +1,16 @@
 class StockLevel < ActiveRecord::Base
+  #Event-B: productmaxthreshold ∈ products → (STOCK_LOCATION → ℕ)
+  #Event-B: productlevels ∈ products →(STOCK_LOCATION → ℕ)
+
   belongs_to :product
   belongs_to :stock_location
   attr_accessible :quantity, :threshold, :product, :stock_location
 
-
-#threshold is max threshold from eventb model
   validates :quantity, :threshold, :numericality => {:greater_than_or_equal_to => 0}
   validates :quantity, :numericality => {:less_than_or_equal_to => :threshold}
+
+  #Event-B: ∀p,l · p∈ activeProducts ∧ l ∈ STOCK_LOCATION ⇒ productmaxthreshold(p)(l) ≥ productthreshold(p)(l)
+  #Need to do it still
 
 
   def below 
