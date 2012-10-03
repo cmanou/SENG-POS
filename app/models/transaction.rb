09343b75a1651cb1128201d81e7257b0da897a73
Lasath Fernando <kde@lasath.org>
2012-10-03 17:54:16 +1000

    Ditch validation for approved in transaction
    It's causing some annoying wierd issues, I'll worry about that
    later. Maybe complain to max.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,5 +12,5 @@ class Transaction < ActiveRecord::Base
   validates :amount, :if => Proc.new {method == 'Cash'},
   	:inclusion => {:in => [0.05, 0.10, 0.20, 0.50, 1.00, 2.00, 5.00, 10.00, 20.00, 50.00, 100.00]}
 
-  validates :approved, :acceptance => true
+#   validates_acceptance_of :approved
 end
