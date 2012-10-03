18f30651a6183b18b7060cdc3aef2d9c21ee1184
Christopher Manouvrier <cmanou@mac.com>
2012-10-03 17:59:54 +1000

    Fixed it needing exact payment

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -111,7 +111,7 @@ class SalesController < ApplicationController
       return
     end
 
-    if @sale.total != @sale.amount_paid
+    if @sale.total > @sale.amount_paid
       redirect_to edit_sale_path(@sale), alert: 'You must finish payment before completing a sale.'
       return
     end
