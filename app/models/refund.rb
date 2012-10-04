827ef31650c5436ef8ad200eab5deb845e83362d
Dylan Kelly <dylank@cse.unsw.edu.au>
2012-10-04 16:35:46 +1000

    validation for refund quantity
    still broken for updates/edits :(

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -6,4 +6,18 @@ class Refund < ActiveRecord::Base
   has_one :sale, :through => :sale_items
   
   attr_accessible :quantity, :reason
+  
+  validate :stock_not_already_returned
+  
+  def stock_not_already_returned
+    @refunds = Refund.find_all_by_sale_item_id(sale_item.id)
+    @quantity_returned = @refunds.sum(&:quantity)
+    @quantity_available = sale_item.quantity - @quantity_returned
+    if sale_item.quantity == 0
+      errors.add(:base, 'Product was not found in sale')
+    elsif quantity > @quantity_available
+      errors.add(:quantity, "of item return is too high. There are only #{@quantity_available} item(s) of this product available to be returned")
+    end
+  end
+  
 end
