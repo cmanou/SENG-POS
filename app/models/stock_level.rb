804948adecdfbd9b5592cca00cf89a1eb8b633fc
Lasath Fernando <kde@lasath.org>
2012-10-05 12:32:02 +1000

    Implement automatic reordering
    From the testing I did so far, everything seems to work about
    right.

--- app/models/stock_level.rb
+++ app/models/stock_level.rb
@@ -9,11 +9,46 @@ class StockLevel < ActiveRecord::Base
   validates :quantity, :threshold, :numericality => {:greater_than_or_equal_to => 0}
   validates :quantity, :numericality => {:less_than_or_equal_to => :threshold}
 
+  after_save :automatic_reorder
+
   #Event-B: ∀p,l · p∈ activeProducts ∧ l ∈ STOCK_LOCATION ⇒ productmaxthreshold(p)(l) ≥ productthreshold(p)(l)
   #Need to do it still
 
 
-  def below 
+  def below
   	quantity < threshold
+  end
+
+  def automatic_reorder
+    if below
+      if stock_location.previous_location
+        #stock transfer
+        exsting_transfer = StockTransfer.find_by_product_id_and_stock_location_id_and_complete(product, stock_location, false)
+        if exsting_transfer
+          exsting_transfer.quantity = threshold - quantity
+          exsting_transfer.save!
+        else
+          StockTransfer.create!(
+            :product => product,
+            :stock_location => stock_location,
+            :quantity => threshold - quantity,
+            :complete => false
+          )
+        end
+      else
+        #supplier stock order
+        existing_order = SupplierStockOrder.find_by_product_id_and_status(product, 'Created')
+        if existing_order
+          existing_order.quantity = threshold - quantity
+          existing_order.save!
+        else
+          SupplierStockOrder.create!(
+            :product => product,
+            :quantity => threshold - quantity,
+            :status => 'Created'
+          )
+        end
+      end
+    end
   end
 end
