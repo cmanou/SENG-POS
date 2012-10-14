97e11d75a02d732f748a33cd31d5be668b303129
Christopher Manouvrier <cmanou@mac.com>
2012-10-14 21:28:11 +1100

    Supplier Stock Order Comments

--- app/controllers/supplier_stock_orders_controller.rb
+++ app/controllers/supplier_stock_orders_controller.rb
@@ -39,9 +39,15 @@ class SupplierStockOrdersController < ApplicationController
 
   # POST /supplier_stock_orders
   # POST /supplier_stock_orders.json
+
+  # Event-b: NewOrder
   def create
+    # Event-b: product ∈ activeProducts
     @product = Product.find(params[:supplier_stock_order][:product])
     params[:supplier_stock_order][:product] = @product
+   
+    #Event-b: act1: orders ≔ orders ∪ {product ↦ quantity}  ›
+    #Event-b: act2: orderStatus ≔ orderStatus ∪ {product ↦ Created}
     @supplier_stock_order = SupplierStockOrder.new(params[:supplier_stock_order])
 
     respond_to do |format|
@@ -57,13 +63,17 @@ class SupplierStockOrdersController < ApplicationController
 
   # PUT /supplier_stock_orders/1
   # PUT /supplier_stock_orders/1.json
+
+  #Event-b: EditOrder
   def update
+    #Event-b: grd1: product ∈ activeProducts
     @product = Product.find(params[:supplier_stock_order][:product])
     params[:supplier_stock_order][:product] = @product
 
     @supplier_stock_order = SupplierStockOrder.find(params[:id])
 
     respond_to do |format|
+      #Event-b: act1: orders ≔ orders <+ {product ↦ quantity}
       if @supplier_stock_order.update_attributes(params[:supplier_stock_order])
         format.html { redirect_to @supplier_stock_order, notice: 'Supplier stock order was successfully updated.' }
         format.json { head :no_content }
@@ -76,6 +86,7 @@ class SupplierStockOrdersController < ApplicationController
 
   # DELETE /supplier_stock_orders/1
   # DELETE /supplier_stock_orders/1.json
+  # Event-b: CancelOrder
   def destroy
     @supplier_stock_order = SupplierStockOrder.find(params[:id])
     @supplier_stock_order.destroy
@@ -87,11 +98,14 @@ class SupplierStockOrdersController < ApplicationController
   end
 
   # GET /supplier_stock_orders/1/process
+
+  #Event-b: UpdateOrderToDelivering
   def process_order
     @supplier_stock_order = SupplierStockOrder.find(params[:id])
 
 
     #Mark as processed
+    #Event-b: act1: orderStatus ≔ orderStatus <+ {product ↦ Delivering}
     @supplier_stock_order.update_attribute(:status,"Processed")
     @supplier_stock_order.save
 
@@ -105,6 +119,7 @@ class SupplierStockOrdersController < ApplicationController
   end
 
   # GET /supplier_stock_orders/1/complete
+  #Event-b: UpdateOrderToComplete & CompleteOrder
   def complete
     @supplier_stock_order = SupplierStockOrder.find(params[:id])
     @product = @supplier_stock_order.product
@@ -112,10 +127,11 @@ class SupplierStockOrdersController < ApplicationController
 
 
     #Mark as processed
+    #Event-b: act1: orderStatus ≔ orderStatus <+ {product ↦ Completed}
     @supplier_stock_order.update_attribute(:status,"Completed")
     @supplier_stock_order.save
 
-
+    #Event-b: productlevels(product) ≔ productlevels(product) <+ {Warehouse ↦ (productlevels(product)(Warehouse) + orders(product))}
     @stock_level = StockLevel.find_by_product_id_and_stock_location_id(@product,@stock_location)
     @stock_level.update_attribute(:quantity, (@stock_level.quantity + @supplier_stock_order.quantity))
     @stock_level.save
