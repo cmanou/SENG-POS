2e0ba134ba608c6ff01d8e233a78fea9c192c10a
Dylan Kelly <dylank@cse.unsw.edu.au>
2012-10-16 08:38:15 +1100

    More validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,20 +26,20 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.role = params[:user][:role]
-    @user.discount = params[:user][:discount]
-    @user.membership = params[:user][:membership]
+    @user.role = params[:role]
+    @user.discount = params[:discount]
+    @user.membership = params[:membership]
 
-    if @user.save
-      respond_to do |format|
+    respond_to do |format|
+      if @user.save     
         format.html { 
           flash[:notice] = 'User was successfully updated.'
           render :edit
         }
         format.json { render json: @user }
-      end
-    else
+      else
         format.html { render action: "edit" }
+      end
     end
   end
 end
