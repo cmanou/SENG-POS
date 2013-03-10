81a50d2d99fa7bc2fc18a396a32b5ffd38bda71a
Christopher Manouvrier <cmanou@mac.com>
2013-03-10 20:40:33 +1100

    Added recipt id to refunds

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -2,8 +2,8 @@ class Refund < ActiveRecord::Base
   belongs_to :sale_item
   belongs_to :checkout_user, :class_name => 'User'
   
-  has_one :products, :through => :sale_items
-  has_one :sale, :through => :sale_items
+  has_one :products, :through => :sale_item
+  has_one :sale, :through => :sale_item
   
   attr_accessible :quantity, :reason
   
